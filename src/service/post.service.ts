c5a9c1385a73a345a947f9281bf3526200e68660
Iababio <innocent.ababio@bobosync.com>
2021-08-06 15:32:27 +0800

    Updated docmentation for sessions functions and schema functions

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -6,19 +6,40 @@ import {
 } from "mongoose";
 import { Post, PostDocument } from "../model";
 
+/**
+ * @function createPost
+ * @description Create a new post
+ * @param {Object} input - The post to create
+ * @returns {Promise<PostDocument | Object | null>} Promise<PostDocument | Object | null>
+ * @memberof PostService
+ */
 export async function createPost(
   input: DocumentDefinition<PostDocument>
 ): Promise<PostDocument | Object | null> {
   return Post.create(input);
 }
 
+/**
+ * @function findPost
+ * @description Find a post by id
+ * @param {ObjectId} id - The id of the post to find
+ * @returns {Promise<PostDocument | Object | null>} Promise<PostDocument | Object | null>
+ * @memberof PostService
+ */
 export async function findPost(
   query: FilterQuery<PostDocument>
 ): Promise<PostDocument> {
   return await Post.findOne(query).lean();
 }
 
-//find All Post
+/**
+ * @function findAllPosts
+ * @description Find all posts
+ * @param {Object} query - The query to find
+ * @param {Object} options - The query options
+ * @returns {Promise<PostDocument[] | Object | null>} Promise<PostDocument[] | Object | null>
+ * @memberof PostService
+ */
 export async function findAllPost(
   query: FilterQuery<PostDocument>,
   options: QueryOptions = { lean: true }
@@ -26,15 +47,29 @@ export async function findAllPost(
   return await Post.find(query, {}, options);
 }
 
-// update posts
-export function findAndUpdate(
+/**
+ * @function updatePost
+ * @description Update a post
+ * @param {ObjectId} id - The id of the post to update
+ * @param {Object} update - The update to perform
+ * @returns {Promise<PostDocument | undefined>} Promise<PostDocument | Object | null>
+ * @memberof PostService
+ */
+export async function findAndUpdate(
   query: FilterQuery<PostDocument>,
   update: UpdateQuery<PostDocument>,
   options: QueryOptions
-): PostDocument | unknown {
-  return Post.findOneAndUpdate(query, update, options);
+): Promise<PostDocument | Object | null> {
+  return await Post.findOneAndUpdate(query, update, options);
 }
 
+/**
+ * @function deletePost
+ * @description Delete a post
+ * @param {ObjectId} id - The id of the post to delete
+ * @returns {Promise<PostDocument | Object | null>} Promise<PostDocument | Object | null>
+ * @memberof PostService
+ */
 export function deletePost(
   query: FilterQuery<PostDocument>
 ): PostDocument | Object | null {
